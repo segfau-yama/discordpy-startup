6bf7cdbb8ce9de230b4ea87334681187ea00ba52
segfau-yama <s1701120@sendai-nct.jp>
2021-08-12 02:30:01 +0900

    ver.0.1.4 リアクプレション集計()

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,7 @@ import re
 from discord.ext import tasks
 from datetime import datetime, timedelta, timezone
 import psycopg2
-import threading
+import time
 
 JST = timezone(timedelta(hours=9))
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -20,28 +20,28 @@ link_regex = re.compile(
     r'(?P<channel_id>[0-9]{15,21})/'
     r'(?P<message_id>[0-9]{15,21})/?$'
 )
-flag_emoji = [
-    "<:C001:873406247108476939>",
-    "<:C002:873402760756879400>",
-    "<:C003:872006469103542332>",
-    "<:C004:873402986062295111>",
-    "<:C005:872008266115350568>",
-    "<:C006:873403184876503101>",
-    "<:C007:872015668860583947>",
-    "<:C008:872449071422537739>",
-    "<:C009:873403429559623720>",
-    "<:C010:872011472002228254>",
-    "<:C011:873403563366301807>",
-    "<:C012:873403690537607299>",
-    "<:C013:872014182961274911>",
-    "<:C014:873403824403017739>",
-    "<:C015:872013712679124992>",
-    "<:C016:872424370230923315>",
-    "<:C017:873405013525930034>",
-    "<:C018:873404987089256449>",
-    "<:C019:873405327104684062>",
-    "<:C020:872010874376818708>"
-]
+flag_emoji = {
+    "<:C001:873406247108476939>":"アメリカーナ帝国",
+    "<:C002:873402760756879400>":"イギリス連合王国",
+    "<:C003:872006469103542332>":"オクター連邦",
+    "<:C004:873402986062295111>":"オーストリア帝国/神聖ローマ帝国",
+    "<:C005:872008266115350568>":"カリフォーニエン大公国",
+    "<:C006:873403184876503101>":"ガリア連邦共和国",
+    "<:C007:872015668860583947>":"グアダラハラ連邦",
+    "<:C008:872449071422537739>":"テソモニア旧共産圏地域",
+    "<:C009:873403429559623720>":"テソモニア民主共和国",
+    "<:C010:872011472002228254>":"デンマーク共和国",
+    "<:C011:873403563366301807>":"ニッショニア連邦",
+    "<:C012:873403690537607299>":"ネーデルランド連合王国",
+    "<:C013:872014182961274911>":"ヒンドゥスターンベンガル武装帝国",
+    "<:C014:873403824403017739>":"ビザンツ帝国",
+    "<:C015:872013712679124992>":"モンゴル自由共和国",
+    "<:C016:872424370230923315>":"ロヘニア",
+    "<:C017:873405013525930034>":"地球連邦",
+    "<:C018:873404987089256449>":"日本国",
+    "<:C019:873405327104684062>":"自由カリブ海同盟",
+    "<:C020:872010874376818708>":"高天原北域商会"
+}
 
 country_and_owner = {
     749896281618645036:"アメリカーナ帝国",
@@ -73,19 +73,47 @@ cur = conn.cursor()
 # 定期メッセージ
 @tasks.loop(seconds=60)
 async def loop():
-    now = datetime.now(tz=JST).strftime('%H:%M')
-    if now == '07:00':
-        channel = client.get_channel(notice_channel)
-        await channel.send('@everyone ごきげんよう、紳士諸君')
-    if now == '15:00':
-        channel = client.get_channel(notice_channel)
-        await channel.send('@everyone お茶会の時間ですわ')
-    if now == '18:00':
-        channel = client.get_channel(notice_channel)
-        await channel.send('@everyone おかえり、紳士諸君')
-    if now == '23:59':
-        channel = client.get_channel(notice_channel)
+    now = datetime.now(tz=JST).strftime('%Y年%m月%d日 %H:%M')
+    pattern = re.compile(r' ')
+    now = pattern.split(now)
+    date = now[0]
+    time = now[1]
+    channel = client.get_channel(notice_channel)
+    v_channel = client.get_channel(vote_channel)
+    if time == '02:50':
         await channel.send('@everyone おやすみ、紳士諸君')
+        vote_message = await v_channel.send(f"@everyone{date}の投票です。一人につき二国まで投票可能です。")
+        print(vote_message.id)
+        for emoji in flag_emoji:
+            await vote_message.add_reaction(emoji)
+        cur.execute("INSERT INTO vote_message (vote_id,message_id,date) VALUES (1,%s,%s);", (vote_message.id, date))
+        conn.commit()
+    if time == '07:00':
+        await channel.send('@everyone ごきげんよう、紳士諸君')
+    if time == '15:00':
+        await channel.send('@everyone お茶会の時間ですわ')
+    if time == '18:00':
+        await channel.send('@everyone おかえり、紳士諸君')
+    if time == '07:00':
+        cur.execute("SELECT * FROM vote_message WHERE vote_id = 1;")
+        data = cur.fetchall()
+        print(data[0][0])
+        message = await v_channel.fetch_message(str(data[0][0]))
+        reactions = message.reactions
+        for reaction in reactions:
+            tp_point = (reaction.count - 1) * 5
+            cur.execute("UPDATE country_user SET transport_point=transport_point+%s WHERE country_name=%s;",
+                        (tp_point, flag_emoji[str(reaction.emoji)]))
+        cur.execute("TRUNCATE TABLE vote_message;")
+        cur.execute("SELECT * FROM country_user;")
+        data = cur.fetchall()
+        country = ""
+        for i in range(20):
+            country += f"{data[i]}\n"
+        print(country)
+        await channel.send(country)
+        conn.commit()
+
 @client.event
 async def on_ready():
     loop.start()
@@ -123,17 +151,19 @@ async def on_message(message):
 
     # クエリ実行機能
     if message.content.startswith("/sql "):
-        result = message.content.replace("/vote ", "")
+        result = message.content.replace("/sql ", "")
         pattern = re.compile(r'/sql+?')
-        result = pattern.split(result)
-        print(result)
-        sql_query = result[1]
-        cur.execute(sql_query)
+        sql_query = pattern.split(result)
+        print(sql_query)
+        cur.execute(sql_query[0])
         conn.commit()
-        data = cur.fetchall()
-        await message.channel.send(data)
+        try:
+            data = cur.fetchall()
+            await message.channel.send(data)
+        except:
+            await message.channel.send("OK")
 
-    # 輸送力加算
+    # 輸送力加算(ニュース)
     if message.channel.id == news_channel:
         result = message.content
         tp_point = len(result) / 50
@@ -142,9 +172,6 @@ async def on_message(message):
         cur.execute("UPDATE country_user SET transport_point=transport_point+%s WHERE country_name=%s;",
                     (tp_point, country_and_owner[message.author.id]))
         conn.commit()
-        cur.execute("SELECT * FROM country_user;")
-        data = cur.fetchall()
-        #await message.channel.send(data)
 
 client.run(token)
 cur.close()

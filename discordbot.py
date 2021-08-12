b4ed695c71a8977b5d49982712954b6d0e0b87fe
segfau-yama <s1701120@sendai-nct.jp>
2021-08-12 10:25:07 +0900

    ver.0.1.7 動作不良デバック2

--- discordbot.py
+++ discordbot.py
@@ -8,6 +8,14 @@ import time
 
 JST = timezone(timedelta(hours=9))
 token = os.environ['DISCORD_BOT_TOKEN']
+develop_channel = 871948382116134922
+client = discord.Client()
+@client.event
+async def on_ready():
+    channel = client.get_channel(develop_channel)
+    await channel.send(token)
+
+'''
 database_url = os.environ['DATABASE_URL']
 notice_channel = 853919175406649364
 vote_channel = 852882836189085697
@@ -80,6 +88,8 @@ async def loop():
     time = now[1]
     channel = client.get_channel(notice_channel)
     v_channel = client.get_channel(vote_channel)
+    d_channel = client.get_channel(develop_channel)
+    await d_channel.send('デバック')
     if time == '02:50':
         await channel.send('@everyone おやすみ、紳士諸君')
         vote_message = await v_channel.send(f"@everyone{date}の投票です。一人につき二国まで投票可能です。")
@@ -117,8 +127,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    d_channel = client.get_channel(develop_channel)
-    await d_channel.send('デバック')
     # 送信者がbotである場合は弾く
     if message.author.bot:
         return
@@ -169,7 +177,7 @@ async def on_message(message):
         cur.execute("UPDATE country_user SET transport_point=transport_point+%s WHERE country_name=%s;",
                     (tp_point, country_and_owner[message.author.id]))
         conn.commit()
-
+'''
 client.run(token)
-cur.close()
-conn.close()
+'''cur.close()
+conn.close()'''

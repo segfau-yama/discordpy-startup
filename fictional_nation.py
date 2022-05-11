671051fcae8de5eb3d16441c4e19f271e1926e0c
segfau-yama <s1701120@sendai-nct.jp>
2022-05-11 20:12:24 +0900

    ver.0.1.8 微調整、TODO追加

--- fictional_nation.py
+++ fictional_nation.py
@@ -15,6 +15,8 @@ class Every:
         self.flag = []
 
     # 起動時処理
+    # TODO:flagの読み込みを関数で行う
+    # TODO:データベースの接続を簡略化する
     @commands.Cog.listener()
     async def on_ready(self):
         self.conn = await asyncpg.connect(self.dsn)
@@ -34,7 +36,7 @@ class Bot(commands.Cog, Every):
         self.loop.cancel()
 
     # 時間処理
-    @tasks.loop(seconds=10)
+    @tasks.loop(seconds=60)
     async def loop(self):
         now = datetime.now().strftime('%H:%M')
         notice_channel = await self.bot.fetch_channel(853919175406649364)
@@ -42,6 +44,7 @@ class Bot(commands.Cog, Every):
         dev_channel = await self.bot.fetch_channel(871948382116134922)
 
         # 投票開始
+        # TODO:投票開始をサブコマンドにする
         if now == "00:00":
             vote = await vote_channel.send('@everyone 人気投票を開始します。投票は2国まで可能です。自国への投票は-10となります。')
             print(vote.id)
@@ -57,7 +60,11 @@ class Bot(commands.Cog, Every):
             await notice_channel.send('おやすみ、紳士諸君')
 
         # 投票集計
+        # TODO:投票集計をサブコマンドにする
+        # TODO:デバッグ
+        # TODO:Embed化
         if now == "23:59":
+            # noinspection PyBroadException
             try:
                 vote_id = None
                 txt = "@everyone 投票結果:\n"
@@ -87,15 +94,17 @@ class Bot(commands.Cog, Every):
                             "UPDATE country SET country_power=country_power+5 WHERE user_id=($1)"
                             , user)
                         pass
-                    await dev_channel.send(txt)
-            except:
+                    await vote_channel.send(txt)
+            except Exception:
                 print("error")
 
     @commands.command()
     async def country_rank(self, ctx):
         """国力ランキングを表示をする"""
         power = "国力ランキング:"
-        rows = await self.conn.fetch("SELECT flag, country_name, user_id, country_power FROM country ORDER BY country_power DESC")
+        rows = await self.conn.fetch(
+            "SELECT flag, country_name, user_id, country_power FROM country ORDER BY country_power DESC"
+        )
         for i, row in enumerate(rows):
             user = await self.bot.fetch_user(row['user_id'])
             power += f"\n{i + 1}位|{row['flag']} | {row['country_name']} | {user} | {row['country_power']}pt\n"
@@ -108,46 +117,27 @@ class Bot(commands.Cog, Every):
         await ctx.send(f"ただいまの時刻:{now}")
 
     @commands.command()
-    async def vote_result(self, ctx):
-        """現在の投票結果を確認する"""
-        vote_channel = await self.bot.fetch_channel(852882836189085697)
-        try:
-            vote_id = None
-            txt = ""
-            rows = await self.conn.fetch("SELECT vote_id FROM bot_data")
-            for row in rows:
-                vote_id = row['vote_id']
-            vote = await vote_channel.fetch_message(int(vote_id))
-            if vote is not None:
-                await ctx.send("現在の投票結果:")
-                for reaction in vote.reactions:
-                    emoji = f"<:{reaction.emoji.name}:{reaction.emoji.id}>"
-                    count = reaction.count * 10 - 10
-                    txt += f"{emoji}:{count}pt\n"
-                await ctx.send(txt)
-        except:
-            print("error")
-
-    @commands.command()
     async def add_meigen(self, ctx, user: discord.Member, meigen: str):
-        """紳士の会メンバーの名言を追加する"""
+        """紳士の会メンバーの迷言を追加する"""
+        await ctx.send(f"迷言を追加しました。\nメンバー:{user}\n名言:{meigen}")
         print(user.id, meigen)
         await self.conn.execute("INSERT INTO meigen (user_id,text) VALUES (($1),($2))", str(user.id), meigen)
 
+    # TODO:迷言の追加の例外処理を明確にする
     @commands.command()
     async def look_meigen(self, ctx, *args):
-        """紳士の会メンバーの名言を見る"""
+        """紳士の会メンバーの迷言を見る"""
         if len(args) == 1:
-            txt = ""
             if '<@' in args[0]:
                 user_id = re.findall('[0-9]+', args[0])
                 rows = await self.conn.fetch("SELECT text FROM meigen WHERE user_id=($1)", user_id[0])
                 user = await self.bot.fetch_user(user_id[0])
+                txt = f"{user}の名言:"
                 for row in rows:
-                    txt += f"{user}の名言:\n{row['text']}\n"
+                    txt += f"\n{row['text']}\n"
                 await ctx.send(txt)
             elif not args[0]:
-                await ctx.send("こいつの名言は無いよ")
+                await ctx.send("こいつの迷言は無いよ")
         elif len(args) == 0:
             txt = ""
             rows = await self.conn.fetch("SELECT text FROM meigen")
@@ -163,26 +153,31 @@ class Administrator(commands.Cog, Every):
     def cog_check(self, ctx):
         return ctx.author.id == 501014325138292737
 
-    @commands.command()
-    async def db_conn(self, ctx):
+    @commands.group()
+    async def db(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('このコマンドにはサブコマンドが必要です。')
+
+    @db.command()
+    async def conn(self, ctx):
         """データベースに再接続する"""
         self.conn = await asyncpg.connect(self.dsn)
         await ctx.send("データベースに接続します")
 
-    @commands.command()
-    async def db_close(self, ctx):
+    @db.command()
+    async def close(self, ctx):
         """データベースから切断する"""
         await ctx.send("データベースから切断します")
         await self.conn.close()
 
-    @commands.command()
-    async def db_query1(self, ctx, *, query):
+    @db.command()
+    async def query1(self, ctx, *, query):
         """クエリを実行する"""
         await ctx.send(query)
         await self.conn.execute(query)
 
-    @commands.command()
-    async def db_query2(self, ctx, *, query):
+    @db.command()
+    async def query2(self, ctx, *, query):
         """クエリを実行し結果を出力する"""
         txt = ""
         rows = await self.conn.fetch(query)
@@ -194,14 +189,15 @@ class Administrator(commands.Cog, Every):
     async def reset(self, ctx):
         """国力をリセットする"""
         await ctx.send("国力を初期化します")
-        await self.conn.execute("UPDATE country SET (country_power, today_vote) = (0, FALSE)")
+        await self.conn.execute("UPDATE country SET country_power = 0")
 
     @commands.command()
     async def add_country(self, ctx, country, flag, player):
         """国家を追加する"""
         await ctx.send(f"国家を追加しました。\n国名:{country}\n国旗:{flag}\nプレイヤーid:{player}")
-        await self.conn.execute("INSERT INTO country (country_name, flag, user_id) VALUES (($1), ($2), ($3))", country,
-                                flag, player)
+        await self.conn.execute(
+            "INSERT INTO country (country_name, flag, user_id) VALUES (($1), ($2), ($3))", country, flag, player
+        )
 
 
 class Test(commands.Cog):
@@ -209,20 +205,27 @@ class Test(commands.Cog):
         self.bot = bot
         self.conn = conn
 
-    @commands.command()
-    async def db_query2(self, ctx, *, query):
-        print(self.conn)
-        txt = ""
-        rows = await self.conn.fetch(query)
-        for row in rows:
-            txt += f"{row}\n"
-        await ctx.send(txt)
+    # メインとなるroleコマンド
+    @commands.group()
+    async def role(self, ctx):
+        # サブコマンドが指定されていない場合、メッセージを送信する。
+        if ctx.invoked_subcommand is None:
+            await ctx.send('このコマンドにはサブコマンドが必要です。')
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print(self.conn)
+    # roleコマンドのサブコマンド
+    # 指定したユーザーに指定した役職を付与する。
+    @role.command()
+    async def add(self, ctx, member: discord.Member, role: discord.Role):
+        await member.add_roles(role)
+
+    # roleコマンドのサブコマンド
+    # 指定したユーザーから指定した役職を剥奪する。
+    @role.command()
+    async def remove(self, ctx, member: discord.Member, role: discord.Role):
+        await member.remove_roles(role)
 
 
 def setup(bot, dsn):
     bot.add_cog(Bot(bot, dsn))
     bot.add_cog(Administrator(bot, dsn))
+    # bot.add_cog(Test(bot, dsn))

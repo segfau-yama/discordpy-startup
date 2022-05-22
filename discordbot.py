8a050c914bc32a2772d3e00e1243ee492f451b09
segfau-yama <s1701120@sendai-nct.jp>
2022-05-22 20:16:20 +0900

    ver.0.2.0 opusバグ解消中

--- discordbot.py
+++ discordbot.py
@@ -5,16 +5,14 @@ import traceback
 import fictional_nation
 import asyncpg
 import asyncio
-
-
 import ctypes
 import ctypes.util
+
+
 print("ctypes -Find opus:")
 a = ctypes.util.find_library('opus')
-print(a)
 print("Discord -Load Opus:")
 b = discord.opus.load_opus(a)
-print(b)
 print("Discord -Is loaded:")
 c = discord.opus.is_loaded()
 print(c)
